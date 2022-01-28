6059f3a3d83fa671adc433de4bf99745a8d67948
Knifey Jay Spoony <knifeyjayspoony@Bradleys-MacBook-Pro.local>
2022-01-28 10:35:00 -0800

    clean up str2tweet

--- str2tweet.py
+++ str2tweet.py
@@ -1,44 +1,28 @@
-from datetime import datetime
+from datetime import datetime, timezone
+from collections import defaultdict
 
 s4sv = """20220130    TEST
 20220131    I bet I'm not the only one who is saddest in January. That's true for many reasons, some of which are unrelated to weather. Of course it's also because we're in the northern hemisphere. When I tell this joke in Sydney, I'm like: "I bet I'm not... saddest in July..."
 20220201    Infinite loops. Amirite?"""
-
-def dt_obj2str(dt_obj):
-    
-    return dt_obj.strftime("%Y%m%d")
-
-
-def nowinutc():
-    ###THIS IS TIMEZONE DEPENDENT
-    return datetime.now()
-
+ 
 def spacesepstr2datestrdict(sss):
-    dsd = {}
+    dsd = defaultdict(list)
     for line in sss.split('\n'):
         d_s, text = line.strip().split('    ')
-        dsd[d_s] = text 
+        dsd[d_s].append(text)
 
     return dsd
 
-def main(sss):
-    now_obj = nowinutc()
-    now_s = dt_obj2str(now_obj)
-
-    text_now_l = []
+def spacesepstr2todaystext_l(sss):
+    now_obj = datetime.now(timezone.utc)
+    now_s = now_obj.strftime("%Y%m%d")
     dsd = spacesepstr2datestrdict(sss)
-    for d_s, txt in dsd.items():
-        if d_s == now_s:
-            text_now_l.append(txt)
-
-    return text_now_l
-
-
-
-
-
+    if now_s in dsd:
+        return dsd[now_s]
+    else:
+        return []
 
 
 if __name__ == "__main__":
-    print(main(s4sv))
+    print(spacesepstr2todaystext_l(s4sv))
 
